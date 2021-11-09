298aa82162fa4bbbd1cb325f676e736e1afe9909
Ihor <dovhanych.ihor@student.uzhnu.edu.ua>
2021-11-09 12:07:58 +0200

    1-3 задачки

---
+++ Lab_7/Task2.py
@@ -0,0 +1,12 @@
+""" Побудувати квадратну матрицю А, елементи якої задаються формулою: """
+def sum(a) :
+    sum_1 = 0  
+    for i in range(a+1) :
+        sum_1 +=((-1)**a)**a
+    return sum_1
+a = []
+j_n = 1
+n=int(input("Введіть розмірність матриці : "))
+for i in range(n) :
+    a.append([(i+j+2 if (i+1)*(1+j)<3 else sum(j)) for j in range(n)])
+print(a)
---
+++ Lab_7/Task3.py
@@ -0,0 +1,12 @@
+a = []
+b = []
+c = [] 
+sum = 0
+n=int(input("Введіть n*n матриці : "))
+for i in range(n) :
+    a.append([float(input("Введіть значення елементу масива A {0} {1}: ".format(i+1,j+1))) for j in range(n)])
+for i in range(n) :
+    b.append([float(input("Введіть значення елементу масива B {0} {1}: ".format(i+1,j+1))) for j in range(n)]) 
+for i in range(n) :
+    c.append([float(a[i][j]+b[i][j]) for j in range(n)])
+print(c) 
