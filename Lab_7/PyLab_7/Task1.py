346bba658c0cc676e0e2a33f148434e5122d9cac
Ihor <dovhanych.ihor@student.uzhnu.edu.ua>
2021-11-10 16:23:56 +0200

    Добавив документацію

--- Lab_7/PyLab_7/Task1.py
+++ Lab_7/PyLab_7/Task1.py
@@ -1,12 +1,12 @@
 """ Визначити добуток від’ємних елементів матриці з обома непарними індексами. """
 A = []
-sum = 0
+sum = 1
 n=int(input("Введіть кількість рядків матриці : "))
 m=int(input("Введіть кількість стовбців матриці : "))
 for i in range(n) :
     A.append([float(input("Введіть значення елементу масива {0}{1}: ".format(i+1,j+1))) for j in range(m)])
     for j in range(m) :
-        if not (j+1)%2==0 :
+        if not (j+1)%2==0 and not (i+1)%2==0 :
             if A[i][j] < 0 :
-                sum += A[i][j]
+                sum *= A[i][j]
 print(sum)
