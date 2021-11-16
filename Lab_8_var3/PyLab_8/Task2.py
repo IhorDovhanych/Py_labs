f8643b9f613137b153dfe76d722d45db3d08a566
Ihor <dovhanych.ihor@student.uzhnu.edu.ua>
2021-11-16 08:04:49 +0200

    -

--- Lab_8_var3/PyLab_8/Task2.py
+++ Lab_8_var3/PyLab_8/Task2.py
@@ -1,9 +1,8 @@
 import math
-
-def funct(x):
-    return math.sqrt(4 * x + math.sin(math.sqrt (x**3) ))
+def func (b):
+    return math.sqrt(4 * b + math.sin(math.sqrt (b**3)))
 def integral(a,b) :
-    return (b-a)*funct(b)
+    return (b-a)*func(b)
 
 x = float(input("Input X : "))
 a = float(input("Input A : "))
